d0a3bd1f708ebd8f5764ee30732021a4395a23ca
AngelViedma <19angelviedma@gmail.com>
2023-12-19 02:19:06 +0100

    validar formularios

--- app/src/main/java/com/example/memorytron/CalculaTron.kt
+++ app/src/main/java/com/example/memorytron/CalculaTron.kt
@@ -2,10 +2,29 @@ package com.example.memorytron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import com.example.memorytron.databinding.ActivityCalculaTronBinding
 
 class CalculaTron : AppCompatActivity() {
+    private lateinit var binding:ActivityCalculaTronBinding
+    private val preferences=AppPreferences(this)
+    private var contador:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calcula_tron)
+        binding=ActivityCalculaTronBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        contador=preferences.getContador()
+        val timer = object: CountDownTimer(contador.toLong(), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvContador.text=(millisUntilFinished/1000).toString()
+            }
+            override fun onFinish() {
+                //Logica al llegar a 0 en contador
+            }
+        }
+        timer.start()
+
     }
 }
--- app/src/main/java/com/example/memorytron/CalculaTron_config.kt
+++ app/src/main/java/com/example/memorytron/CalculaTron_config.kt
@@ -1,11 +1,94 @@
 package com.example.memorytron
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.memorytron.databinding.ActivityCalculaTronConfigBinding
 
 class CalculaTron_config : AppCompatActivity() {
+    private lateinit var binding: ActivityCalculaTronConfigBinding
+    private val preferences = AppPreferences(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calcula_tron_config)
+        binding = ActivityCalculaTronConfigBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        initListeners()
+
+
     }
+
+    private fun initListeners() {
+        binding.btGuardarConfig.setOnClickListener {
+            if (validarConfig()) {
+                preferences.setContador(convertFromSecondsToMillis(binding.textContador.text.toString()))
+                preferences.setMaximo(binding.textMax.text.toString().toFloat())
+                preferences.setMinimo(binding.textMinimo.text.toString().toFloat())
+                //ISSELECTED O ISCHECKED???
+                preferences.setIsSuma(binding.checkSuma.isSelected)
+                preferences.setIsResta(binding.checkResta.isSelected)
+                preferences.setIsMulti(binding.checkMultiplicacion.isSelected)
+
+                Toast.makeText(this, "Configuracion guardada", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, CalculaTron::class.java))
+            }
+        }
+    }
+
+    private fun validarConfig(): Boolean {
+        var validar = true
+
+        if (binding.textContador.text.toString().isNullOrEmpty()) {
+            binding.textInputLayoutCuentaAtras.error = "El contador no puede estar vacio"
+            validar = false
+        } else {
+            binding.textInputLayoutCuentaAtras.error = null
+        }
+
+
+        if (binding.textMinimo.text.toString().isNullOrEmpty()) {
+            binding.textInputLayoutMinimo.error = "El minimo no puede estar vacio"
+            validar = false
+        } else {
+            binding.textInputLayoutMinimo.error = null
+        }
+        if (binding.textMinimo.text.toString().contains("-")
+        ) {
+            binding.textInputLayoutMinimo.error =
+                "El minimo solo permite numeros positivos y sin espacios"
+            validar = false
+        } else {
+            binding.textInputLayoutMinimo.error = null
+        }
+
+        if (binding.textMax.text.toString().isNullOrEmpty()) {
+            binding.layoutMax.error = "El maximo no puede estar vacio"
+            validar = false
+        } else {
+            binding.layoutMax.error = null
+        }
+
+        if (binding.textMax.text.toString().contains("-")
+        ) {
+            binding.layoutMax.error = "El maximo solo permite numeros positivos y sin espacios"
+        } else {
+            binding.layoutMax.error = null
+        }
+
+        if (!binding.checkResta.isChecked && !binding.checkSuma.isChecked && !binding.checkMultiplicacion.isChecked) {
+            binding.layoutCheckBox.error = "No has seleccionado ninguna operacion."
+            validar = false
+        } else {
+            binding.layoutCheckBox.error = null
+        }
+        return validar
+    }
+
+    private fun convertFromSecondsToMillis(seconds: String): Int {
+        return (seconds.toFloat() * 1000).toInt()
+    }
+
+
 }
